81a66c4e46e115e73658a143304b623ee0408ee7
japch <japch@LAPTOP-K6TO851M>
2020-11-25 19:15:59 -0800

    First commit!

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Git demo project");
+		System.out.println("Github is online storage for git repository");
+
+	}
+
+}
